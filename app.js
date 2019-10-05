6d9a214ae6d004e599b05ba731e2c8fdce952565
Eugene <eleonkov@gmail.com>
2019-10-05 22:20:08 +0300

    add some

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const blackList = ["2252680122", "6218218597", "6367032077", "4095468927",
 const stopWords = ['via', 'dm', 'tag', 'follow', 'pic', 'подпишись',
     'repost', 'tap', 'credits', 'credit', 'siga', 'подписывайся', 'forza',
     'visit', 'bio', 'golf', 'bmw', 'япония', 'www', 'рассрочка', 'opel',
-    'honda', 'ford', 'audi', 'volks'
+    'honda', 'ford', 'audi', 'volks', 'ferrari', 'mazda'
 ];
 
 const accessibilityWords = ['text', 'текс', 'человек', 'люди', 'people'];
